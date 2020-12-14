d2459d46311af80f6839702ea0c28b074ac91b5f
Bonumex <prawdziwezycie.biznes@gmail.com>
2020-12-14 21:17:51 +0100

    add divs in htlm file and make method whose add id to divs

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const container = document.querySelector(".container");
+const allDivs = document.querySelectorAll(".squere");
 // const createSquare =  document.createElement('div');
+let counter = 0;
 
-function makeSide() {
-    for (let i = 0; i < 10; i++){
-        container.appendChild("div");
-        console.log("dziala");
-    }
+console.log(container);
+// iteruje nam wszystkie divy trzeba dodac data
+allDivs.forEach((pool) => {
+    pool.id = `s+${counter}`;
+    counter++;
+});
+
+function writeSign(){
+    console.log('dziala')
 }
-makeSide();
+
+allDivs.forEach.addEventListener("click", writeSign);
+//1 Zrobic funkcjie ktora ta data base z id zeby mozna bylo identyfikowac pola 
+
+//2 Zrobic obsluge tych pol za pomoca eventListener i ustwic go na click
+
+//3 zroobic logike ktora raz jest x a raz 0 
+
+// 4 Zrobic logike aby ten kto da 3 x albo 3 0 ten wygrywa zza pomoca instrukcji warunkowych 
